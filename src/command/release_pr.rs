3fe8c68b59cdb12f84a85b5551a71db29cc552d3
Rob Gonnella <robgonnella@gmail.com>
2025-08-27 23:27:00 -0400

    refactor: minor refactoring of release_pr command mod

--- src/command/release_pr.rs
+++ src/command/release_pr.rs
@@ -9,9 +9,11 @@ use crate::{
         git_cliff::GitCliffChangelog,
         traits::{Output, Writer},
     },
-    cli, config,
+    cli,
+    config::{self, CliSinglePackageConfig},
     forge::{
-        config::{DEFAULT_PR_BRANCH_PREFIX, PENDING_LABEL},
+        config::{DEFAULT_PR_BRANCH_PREFIX, PENDING_LABEL, RemoteConfig},
+        traits::Forge,
         types::{
             CreatePrRequest, GetPrRequest, PrLabelsRequest, UpdatePrRequest,
         },
@@ -19,61 +21,90 @@ use crate::{
     git::Git,
 };
 
-pub fn execute(args: &cli::Args) -> Result<()> {
-    let remote = args.get_remote()?;
-    let forge = remote.get_forge()?;
-    let remote_config = forge.config();
-    let tmp_dir = TempDir::new()?;
+fn process_package(
+    config: &CliSinglePackageConfig,
+    remote_config: &RemoteConfig,
+) -> Result<Output> {
+    let changelog = GitCliffChangelog::new(ChangelogConfig {
+        body: config.changelog.body.clone(),
+        header: config.changelog.header.clone(),
+        footer: config.changelog.footer.clone(),
+        package: PackageConfig {
+            name: config.package.name.clone(),
+            path: config.package.path.clone(),
+            tag_prefix: config.package.tag_prefix.clone(),
+        },
+        commit_link_base_url: remote_config.commit_link_base_url.clone(),
+        release_link_base_url: remote_config.release_link_base_url.clone(),
+    })?;
+    changelog.write()
+}
 
-    info!(
-        "cloning repository {} to {}",
-        remote_config.repo,
-        tmp_dir.path().display()
-    );
-    let git = Git::new(tmp_dir.path(), remote_config.clone())?;
-
-    info!(
-        "switching directory to cloned repository: {}",
-        tmp_dir.path().display(),
-    );
-    env::set_current_dir(tmp_dir.path())?;
-
-    info!("loading configuration");
-    let cli_config = config::load_config()?;
-
-    let release_branch =
-        format!("{}{}", DEFAULT_PR_BRANCH_PREFIX, git.default_branch);
-
-    git.create_branch(&release_branch)?;
-    git.switch_branch(&release_branch)?;
-
-    let mut manifest: HashMap<String, Output> = HashMap::new();
-
-    for single in cli_config {
-        let name = single.package.name.clone();
-        let changelog = GitCliffChangelog::new(ChangelogConfig {
-            body: single.changelog.body.clone(),
-            header: single.changelog.header.clone(),
-            footer: single.changelog.footer.clone(),
-            package: PackageConfig {
-                name: single.package.name.clone(),
-                path: single.package.path.clone(),
-                tag_prefix: single.package.tag_prefix.clone(),
-            },
-            commit_link_base_url: remote_config.commit_link_base_url.clone(),
-            release_link_base_url: remote_config.release_link_base_url.clone(),
-        })?;
-        let output = changelog.write()?;
-        if name.is_empty() {
-            manifest.insert(single.package.path, output);
-        } else {
-            manifest.insert(name, output);
-        }
-    }
-
+fn create_or_update_pr(
+    title: &str,
+    body: &str,
+    release_branch: &str,
+    git: &Git,
+    forge: Box<dyn Forge>,
+) -> Result<()> {
     let head_branch =
         format!("{}{}", DEFAULT_PR_BRANCH_PREFIX, git.default_branch);
     let base_branch = git.default_branch.clone();
+
+    git.add_all()?;
+    git.commit(title)?;
+    git.push_branch(release_branch)?;
+
+    let req = GetPrRequest {
+        head_branch: head_branch.clone(),
+        base_branch: base_branch.clone(),
+    };
+
+    let result = forge.get_pr_number(req)?;
+
+    let pr_number = match result {
+        Some(pr) => {
+            info!("updating pr {pr}");
+            let req = UpdatePrRequest {
+                pr_number: pr,
+                body: body.to_string(),
+            };
+
+            forge.update_pr(req)?;
+            pr
+        }
+        None => {
+            info!("creating pull request: {title}");
+            let req = CreatePrRequest {
+                title: title.to_string(),
+                body: body.to_string(),
+                head_branch,
+                base_branch,
+            };
+
+            forge.create_pr(req)?
+        }
+    };
+
+    info!("setting labels for pr {pr_number}");
+    let req = PrLabelsRequest {
+        pr_number,
+        labels: vec![PENDING_LABEL.into()],
+    };
+
+    forge.replace_pr_labels(req)
+}
+
+struct ManifestProcessingResult {
+    pub title: String,
+    pub body: Vec<String>,
+    pub releasable: bool,
+}
+
+fn process_manifest(
+    manifest: HashMap<String, Output>,
+    git: &Git,
+) -> ManifestProcessingResult {
     let mut title = format!("chore({}): release", git.default_branch);
     let mut body = vec![];
     let mut releasable = false;
@@ -95,57 +126,76 @@ pub fn execute(args: &cli::Args) -> Result<()> {
         }
     }
 
-    info!("creating pr");
-    info!("title: {title}");
+    ManifestProcessingResult {
+        title,
+        body,
+        releasable,
+    }
+}
+
+pub fn execute(args: &cli::Args) -> Result<()> {
+    let remote = args.get_remote()?;
+    let forge = remote.get_forge()?;
+    let remote_config = forge.config();
+    let tmp_dir = TempDir::new()?;
+
+    info!(
+        "cloning repository {} to {}",
+        remote_config.repo,
+        tmp_dir.path().display()
+    );
+
+    let git = Git::new(tmp_dir.path(), remote_config.clone())?;
+
+    info!(
+        "switching directory to cloned repository: {}",
+        tmp_dir.path().display(),
+    );
+
+    env::set_current_dir(tmp_dir.path())?;
+
+    info!("loading configuration");
+
+    let cli_config = config::load_config()?;
+
+    let release_branch =
+        format!("{}{}", DEFAULT_PR_BRANCH_PREFIX, git.default_branch);
+
+    debug!("setting up release branch: {release_branch}");
+    git.create_branch(&release_branch)?;
+    git.switch_branch(&release_branch)?;
+
+    let mut manifest: HashMap<String, Output> = HashMap::new();
+
+    for single in cli_config {
+        debug!(
+            "processing changelog for package path: {}",
+            single.package.path
+        );
+        let name = single.package.name.clone();
+        let output = process_package(&single, remote_config)?;
+        if name.is_empty() {
+            manifest.insert(single.package.path, output);
+        } else {
+            manifest.insert(name, output);
+        }
+    }
+
+    let manifest_result = process_manifest(manifest, &git);
 
     if args.dry_run {
         info!("dry-run: skipping remote update");
         return Ok(());
     }
 
-    if releasable {
-        git.add_all()?;
-        git.commit(&title)?;
-        git.push_branch(&release_branch)?;
-
-        let req = GetPrRequest {
-            head_branch: head_branch.clone(),
-            base_branch: base_branch.clone(),
-        };
-
-        let result = forge.get_pr_number(req)?;
-
-        let pr_number = match result {
-            Some(pr) => {
-                info!("updating pr {pr}");
-                let req = UpdatePrRequest {
-                    pr_number: pr,
-                    body: body.join("\n"),
-                };
-
-                forge.update_pr(req)?;
-                pr
-            }
-            None => {
-                info!("creating pull request");
-                let req = CreatePrRequest {
-                    title,
-                    body: body.join("\n\n"),
-                    head_branch,
-                    base_branch,
-                };
-
-                forge.create_pr(req)?
-            }
-        };
-
-        info!("setting labels for pr {pr_number}");
-        let req = PrLabelsRequest {
-            pr_number,
-            labels: vec![PENDING_LABEL.into()],
-        };
-
-        forge.replace_pr_labels(req)?;
+    if manifest_result.releasable {
+        create_or_update_pr(
+            &manifest_result.title,
+            &manifest_result.body.join("\n"),
+            &release_branch,
+            &git,
+            forge,
+        )?;
     }
 
     Ok(())
