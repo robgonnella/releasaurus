fd67a6865d27fdb78c4b883ba24049d2c2c65d1e
robgonnella <robgonnella@users.noreply.github.com>
2026-02-01 18:59:24 +0000

    deploy: 58d50f58fb418bdbb143d318333d5c73de1daf36

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0c3c4f41.js',
+                path_to_root + 'searchindex-722411d6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
