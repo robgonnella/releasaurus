679081ca7792cab0cd88cc5c4f71137b7f27f97a
robgonnella <robgonnella@users.noreply.github.com>
2026-05-09 11:59:10 +0000

    deploy: 6eed248152e7e982612dca1c22b58a4415957752

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8f3af27d.js',
+                path_to_root + 'searchindex-951f73fa.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
