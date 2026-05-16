5d81b3acead37cddc2b31984dd247469cbb52fdf
Rob Gonnella <robgonnella@gmail.com>
2026-05-15 22:23:52 -0400

    fix: fixes forgejo implementations
    Forgejo doesn't support force pushingo on the /contents routes yet. So
    this commit addresses that issue by diverging from the gitea
    implementation for the create_release_branch and create_commit methods

---
+++ crates/core/src/forge/forgejo/types.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+
+use crate::forge::request::Commit;
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ForgejoFileChangeOperation {
+    Create,
+    Update,
+}
+
+#[derive(Debug, Serialize)]
+pub struct ForgejoFileChange {
+    pub path: String,
+    pub content: String,
+    pub operation: ForgejoFileChangeOperation,
+    pub sha: Option<String>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct ForgejoModifyFiles {
+    pub branch: String,
+    pub new_branch: Option<String>,
+    pub message: String,
+    pub files: Vec<ForgejoFileChange>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ForgejoCreatedCommit {
+    pub commit: Commit,
+}
